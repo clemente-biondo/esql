21d96876c6f052997605106319f7908dcf62988c
Clemente Biondo <clemente.biondo@gmail.com>
2018-04-14 18:51:03 +0200

    first draft of the ui

---
+++ src/main/resources/static/esql/query-list/query-list.component.js
@@ -0,0 +1,8 @@
+'use strict';
+angular.module('esqlApp').component('queryList', {
+  templateUrl: 'query-list/query-list.template.html',
+    controller: [function QueryListController() {
+
+    }
+  ]
+});
